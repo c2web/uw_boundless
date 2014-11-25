6ec091930c2a1043343d75112590a087664a4550
jbleys <jbleys@JBLEYS-MAC.local>
2014-11-24 18:19:41 -0800

    more scaffolding of the page template

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -86,11 +86,8 @@
 </div>
 <!-- /#uwsearcharea -->
 
-    <?php //print render($page['search']); ?>
-
 <div id="uw-container">
 
-    <!-- #todo  build quicklinks dynamically -->
     <nav id="quicklinks" role="navigation" aria-label="quick links" aria-hidden="" class="">
         <ul id="big-links"> 
             <li><span class="icon-myuw"></span><a href="http://myuw.washington.edu" tabindex="0">MyUW</a></li> 
@@ -113,55 +110,17 @@
         </ul>
     </nav>
     
-    
-    <!--<a href='#main_content' class='screen-reader-shortcut' tabindex=1>Skip to main content</a>-->
-
     <div id="uw-container-inner">
-        
-        <header class="uw-thinstrip">
-
-        <nav class="uw-thin-strip-nav" role='navigation' aria-label='audience based'>
-            <ul class="uw-thin-links">
-              <li class="uw-quicklinks"><button aria-haspopup="true" aria-expanded="">Quick Links<svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="15.63px" height="69.13px" viewBox="0 0 15.63 69.13" enable-background="new 0 0 15.63 69.13" xml:space="preserve"><polygon fill="#FFFFFF" points="12.8,7.776 12.803,7.773 5.424,0 3.766,1.573 9.65,7.776 3.766,13.98 5.424,15.553 12.803,7.78"/><polygon fill="#FFFFFF" points="9.037,61.351 9.036,61.351 14.918,55.15 13.26,53.577 7.459,59.689 1.658,53.577 0,55.15 5.882,61.351 5.882,61.351 5.884,61.353 0,67.557 1.658,69.13 7.459,63.019 13.26,69.13 14.918,67.557 9.034,61.353"/></svg></button></li>
-              <li class="uw-search"><button aria-haspopup='true' aria-owns='uwsearcharea' aria-controls='uwsearcharea' aria-expanded=''>Open search area
-
-              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                 width="18.776px" height="51.062px" viewBox="0 0 18.776 51.062" enable-background="new 0 0 18.776 51.062" xml:space="preserve">
-              <g>
-                <path fill="#FFFFFF" d="M3.537,7.591C3.537,3.405,6.94,0,11.128,0c4.188,0,7.595,3.406,7.595,7.591
-                  c0,4.187-3.406,7.593-7.595,7.593C6.94,15.185,3.537,11.778,3.537,7.591z M5.245,7.591c0,3.246,2.643,5.885,5.884,5.885
-                  c3.244,0,5.89-2.64,5.89-5.885c0-3.245-2.646-5.882-5.89-5.882C7.883,1.71,5.245,4.348,5.245,7.591z"/>
-
-                  <rect x="2.418" y="11.445" transform="matrix(0.7066 0.7076 -0.7076 0.7066 11.7842 2.0922)" fill="#FFFFFF" width="1.902" height="7.622"/>
-              </g>
-              <path fill="#FFFFFF" d="M3.501,47.864c0.19,0.194,0.443,0.29,0.694,0.29c0.251,0,0.502-0.096,0.695-0.29l5.691-5.691l5.692,5.691
-                c0.192,0.194,0.443,0.29,0.695,0.29c0.25,0,0.503-0.096,0.694-0.29c0.385-0.382,0.385-1.003,0-1.388l-5.692-5.691l5.692-5.692
-                c0.385-0.385,0.385-1.005,0-1.388c-0.383-0.385-1.004-0.385-1.389,0l-5.692,5.691L4.89,33.705c-0.385-0.385-1.006-0.385-1.389,0
-                c-0.385,0.383-0.385,1.003,0,1.388l5.692,5.692l-5.692,5.691C3.116,46.861,3.116,47.482,3.501,47.864z"/>
-              </svg>
-
-            </button></li>
-              <li><a href="http://uw.edu/alumni" title="Alumni">Alumni</a></li>
-              <li><a href="http://uw.edu/facultystaff" title="Faculty & Staff">Faculty & Staff</a></li>
-              <li><a href="http://uw.edu/parents" title="Parents">Parents</a></li>
-                 <li><a href="http://uw.edu/studentlife" title="Students">Students</a></li>
-            </ul>
-        </nav>
-
-        <div class="container">
-          <a href="http://uw.edu" title="University of Washington Home" class="uw-patch" tabindex='-1' aria-hidden='true'>Home</a>
-          <a href="http://uw.edu" title="University of Washington Home" class="uw-wordmark">Home</a>
-        </div>
-      </header><!-- /#uw-thinstrip -->
+        <?php include "includes/thinstrip.php"; ?>
+        <!-- /#uw-thinstrip -->
         
         <div class="container uw-body">
             
             <header role="banner" id="page-header">
-              <?php if (!empty($site_slogan)): ?>
-                <p class="lead"><?php print $site_slogan; ?></p>
-              <?php endif; ?>
 
-              <?php print render($page['header']); ?>
+                <?php print render($page['header']); ?>
+                
+                <div class="uw-hero-image" style="background-image:url('http://www.washington.edu/brand/files/2014/09/w3.jpg');"></div>
             </header> <!-- /#page-header -->
             
             <div class="row">
@@ -170,7 +129,14 @@
                 <?php if (!empty($page['highlighted'])): ?>
                   <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
                 <?php endif; ?>
+                  
                 <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+                <?php //if (!empty($breadcrumb)): ?>
+                  <!--<nav class="uw-breadcrumbs" role="navigation" aria-label="breadcrumbs">
+                      <?php //print $breadcrumb; ?>
+                  </nav>-->
+                <?php //endif; ?>
+                  
                 <a id="main-content"></a>
                 <?php print render($title_prefix); ?>
                 <?php if (!empty($title)): ?>
@@ -188,6 +154,13 @@
                   <ul class="action-links"><?php print render($action_links); ?></ul>
                 <?php endif; ?>
                 <div class="uw-content" role='main'>
+                    <?php if (!empty($site_name)): ?>
+                        <a href="<?php print $front_page; ?>" title="<?php print $site_name; ?>"><h2 class="uw-site-title"><?php print $site_name; ?></h2>
+                        <?php if (!empty($site_slogan)): ?>
+                            <h2 class="uw-site-title" style="top:-95px"><?php print $site_slogan; ?></h2>
+                        </a>
+                        <?php endif; ?>
+                    <?php endif; ?>
                     <div id='main_content' class="uw-body-copy">
                     <?php print render($page['content']); ?>
                     </div>
