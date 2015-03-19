05f1f321233f2ef40d1df67ef1e468281364aab2
jbleys <jbleys@JBLEYS-MAC.local>
2015-03-19 16:18:12 -0700

    force IE to open page in standards mode

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -48,6 +48,8 @@
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces;?>>
 <head profile="<?php print $grddl_profile; ?>">
   <meta charset="utf-8">
+  <!-- force IE to open page in standards mode -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
